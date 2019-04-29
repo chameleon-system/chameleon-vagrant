6b4401dee73036e9f87fe41e09c0771203dc08fd
Maximilian Heinrich <maximilian.heinrich@esono.de>
2019-04-29 10:20:01 +0200

    switch to php 7.2, add config

--- bootstrap.sh
+++ bootstrap.sh
@@ -37,9 +37,41 @@ chmod +x /usr/local/bin/docker-compose
 echo "Step [3/6] - Installing chameleon sources..."
 mkdir -p /home/wwwusers/chameleon
 cd /home/wwwusers/chameleon
-git clone https://github.com/chameleon-system/chameleon-system.git customer
-git clone https://github.com/chameleon-system/chameleon-resources.git resources
-# TODO parameters.yml creation
+
+curl -Ls https://github.com/chameleon-system/chameleon-system/archive/7.0.0.tar.gz -o chameleon.tar.gz
+tar xfz chameleon.tar.gz
+mv chameleon-system-7.0.0 customer
+
+curl -Ls https://github.com/chameleon-system/chameleon-resources/archive/7.0.0.tar.gz -o chameleon-resources.tar.gz
+tar xfz chameleon-resources.tar.gz
+mv chameleon-resources-7.0.0 resources
+
+# add custom config
+# this should be set via env as soon as possible via composer or alike
+
+cat << EOF > /home/wwwusers/chameleon/customer/app/config/parameters.yml
+parameters:
+    secret: ChangeMe
+    chameleon_system_core.debug.show_view_source_html_hints: true
+    chameleon_system_core.development_email: 'email@example-com'
+    database_host: mysql
+    database_port: 3306
+    database_name: chameleon
+    database_user: chameleon
+    database_password: chameleon
+    chameleon_system_core.cache.memcache_server1: memcached
+    chameleon_system_core.cache.memcache_port1: '11211'
+    chameleon_system_core.cache.memcache_sessions_server1: memcached-session
+    chameleon_system_core.cache.memcache_sessions_port1: '11211'
+    chameleon_system_core.mail_target_transformation_service.enabled: false
+    chameleon_system_core.mail_target_transformation_service.subject_prefix: '[TEST] '
+    chameleon_system_core.mail_target_transformation_service.target_mail: mail@example.com
+    chameleon_system_core.mail_target_transformation_service.white_list: '@@PORTAL-DOMAINS'
+    mailer_host: 127.0.0.1
+    mailer_user: null
+    mailer_password: null
+EOF
+
 chown -R www-data:www-data /home/wwwusers/chameleon
 
 echo "Step [4/6] - Launching docker-compose application stack..."
@@ -53,10 +85,16 @@ chown www-data:www-data /home/vagrant/installcomposer.sh
 docker exec -i $(docker ps | grep 'php' | awk '{print $1}') chown www-data:www-data /var/www
 docker exec -u www-data -i $(docker ps | grep 'php' | awk '{print $1}') /usr/local/bin/installcomposer.sh
 
-echo "Step [6/6] - Importing database..."
-# TODO import database
+echo "Step [6/6] - Importing database and mediapool..."
+# importing DB
 MYSQLDOCKER=$(docker ps | grep 'mariadb' | awk '{print $1}')
-docker exec -ti ${MYSQLDOCKER} bash -c 'cat /initial-database/*.sql | mysql -uchameleon -pchameleon chameleon'
+docker exec -i ${MYSQLDOCKER} bash -c 'cat /initial-database/*.sql | mysql -uchameleon -pchameleon chameleon'
+
+# setting primary domain
+docker exec -i ${MYSQLDOCKER} /bin/bash -c "echo \"INSERT INTO cms_portal_domains SET id='1', name='localhost', cms_portal_id='1', is_master_domain='1';\" | mysql -u chameleon -pchameleon chameleon"
+
+# importing mediapool
+rsync -a /home/wwwusers/chameleon/resources/mediapool/ /home/wwwusers/chameleon/customer/web/chameleon/mediapool/
 
 echo "Ready! You should now be able to access chameleon at the given hostname. Remember restrictions for privileged ports!"
 # close fd=3
